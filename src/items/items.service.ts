60df840d80b03898bc73c2453bd6edc0e10ed0c6
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-05-31 11:20:31 +0900

    授業番号21　CRUD機能の実装（Update）

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Item } from './item.model';
+import { ItemStatus } from './item-status.enum';
 
 @Injectable()
 export class ItemsService {
@@ -14,6 +15,11 @@ export class ItemsService {
 
   create(item: Item): Item {
     this.items.push(item);
+    return item;
+  }
+  updateStatus(id: string): Item {
+    const item = this.findById(id);
+    item.status = ItemStatus.SOLD_OUT;
     return item;
   }
 }
