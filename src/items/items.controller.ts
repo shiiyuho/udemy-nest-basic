0bc8bb15c4201e5552654356eedb95348d383767
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-06-03 15:24:56 +0900

    授業番号37　DBマイグレーション

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -14,6 +14,7 @@ import { CreateItemDto } from './dto/create-item.dto';
 
 @Controller('items')
 export class ItemsController {
+  // eslint-disable-next-line no-unused-vars
   constructor(private readonly itemsService: ItemsService) {}
   @Get()
   findAll(): Item[] {
