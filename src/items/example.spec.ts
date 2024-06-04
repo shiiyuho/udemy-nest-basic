cfd19acb6ae3f73562e4200e45f6268b1131fa76
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-06-04 16:48:58 +0900

    授業番号60　jestによるユニットテスト

---
+++ src/items/example.spec.ts
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+describe('Jestの練習', () => {
+  it('test1', () => {
+    const result = 1;
+    const exprected = 2;
+    expect(result).toEqual(exprected);
+    expect(result).toEqual(exprected);
+  });
+});
